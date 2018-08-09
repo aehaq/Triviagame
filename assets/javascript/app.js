e8f53cdaf56a5ee4ab9d36ee74bf4541c256dcd8
aehaq <azfarehaq@gmail.com>
2018-08-08 23:30:23 -0700

    added variables and incomplete functions to js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,3 +25,54 @@ var quotesKhalid = [
 ];
 
 
+var correct;
+var incorrect;
+var intervalId;
+var timeLeft =  10;
+var print = $('.main');
+
+//This function begins the timer for the game.
+function countdown() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000)
+}
+
+//This function is called upon within the countdown function every second, 
+//it decreases and prints the current time left, and signals the end of the game when the time left is zero.
+function decrement() {
+    timeLeft--;
+    $("#clock").text(timeLeft)
+    console.log(timeLeft);
+    if (timeLeft ===0) {
+        clearInterval(intervalId);
+        endGame();
+    }
+}
+
+//This function tells the game what to do when it ends.
+function endGame() {
+    console.log("Game over!")
+}
+
+//This event covers the start of the game, upon click of the start button.
+$("#start").on("click", function() {
+    //removes button and preps area for more content
+    print.empty();
+
+    //prints the countdown timer and provides an id for the decrement function to overwrite.
+    print.append('<h2>Time Left: <span id="clock">120</span></h2>');
+
+    //prints the button to mark completion of the quiz
+    print.append('<button type="button" id="finish">Finished!</button>')
+
+    //Countdown for the quiz begins
+    countdown();
+})
+
+//This event is called upon clicking the finish button during the games run.
+//Stops the timer to prevent the endgame function from running a second time,
+//signals the end of the game
+$("#finish").on("click", function() {
+    clearInterval(intervalId);
+    endGame()
+})
