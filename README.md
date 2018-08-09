af6dfa7d71e09fbf30f388612fafc4586ce8a2a3
aehaq <azfarehaq@gmail.com>
2018-08-09 14:56:17 -0700

    final edits and readme

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # Triviagame
+A simple timed trivia game with binary choices for answers.
+
+# Link to Deployed Site
+[Trivia-game](https://aehaq.github.io/Triviagame/)
+
+# Images
+![Preview](assets/images/preview.PNG)
+
+# Technology Used
+- html
+- CSS (Bootstrap)
+- javascript (jQuery)
+
+# Code Snippets
+```
+    $("#start").on("click", function() {
+        ...
+
+        for (let i = 0; i < 10; i++) {
+
+            print.append('<hr class="my-2"><p class="question">"' + quotes[i] + '"</p>');
+
+            if (khalidQuote[i]) {
+                print.append('<form>    <input type="radio" name="q'+i+'" value="right"> DJ Khaled    <input type="radio" name="q'+i+'" value="wrong"> US President </form>')
+            } else {
+                print.append('<form>    <input type="radio" name="q'+i+'" value="wrong"> DJ Khaled    <input type="radio" name="q'+i+'" value="right"> US President </form>')
+            }
+        }
+
+        ...
+    })
+
+```
+In the code above I attempted to cover a lot of ground in a few lines of code. After clearing the page and printing the clock within the above function (cropped out of this snippet), I need to print 10 different questions, each with a set of two different radio buttons with hidden values of either correct or incorrect. In order to do this I simultaneously accessed two corresponding lists within the same for loop, one list containing the questions and one list containing the answers. After printing each question, I then determine which of the two results is true, and print the buttons on page with the correct values already assigned. While doing this, I also made sure to give each set of radio buttons a unique id corresponding to the question they respond to by calling the index number within the printed html.
+
+# Learning Points
+- Creating timed events, in this case a countdown clock, where a function is run every second, and a function is triggered when a certain time has been reached.
+- Creating events that interact with timed functions, such as stopping a countdown to prevent excess instances of functions running..
+- Clearing and writing in certain divs upon the execution of certain functions and the instancing of certain events.
+- Using a for loop to procedurally generate various elements according to details stored in multiple lists.
+- Using radio-buttons to handle and store binary values, and calling upon those values to record data.
+- Identifying the best instances to store values when you need to remove associated elements from the page.
+
+# Author 
+Azfar Haq - [GitHub](https://github.com/aehaq)
+
+# License
+Standard MIT License
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,92 +24,98 @@ var khalidQuote = [
     true
 ];
 
+$( document ).ready(function() {
 
-var correct = 0;
-var incorrect = 0;
-var unanswered = 0;
-var intervalId;
-var timeLeft =  90;
-var print = $('.main');
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+    var intervalId;
+    var timeLeft =  90;
+    var print = $('.main');
 
-//This function begins the timer for the game.
-function countdown() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000)
-}
-
-//This function is called upon within the countdown function every second, 
-//it decreases and prints the current time left, and signals the end of the game when the time left is zero.
-function decrement() {
-    timeLeft--;
-    $("#clock").text(timeLeft)
-    console.log(timeLeft);
-    if (timeLeft ===0) {
+    //This function begins the timer for the game.
+    function countdown() {
         clearInterval(intervalId);
-        endGame();
+        intervalId = setInterval(decrement, 1000)
     }
-}
 
-//This event covers the start of the game, upon click of the start button.
-$("#start").on("click", function() {
-    //removes button and preps area for more content
-    print.empty();
-
-    //prints the countdown timer and provides an id for the decrement function to overwrite.
-    print.append('<h2>Time Left: <span id="clock">90</span></h2>');
-
-    //this for loop will print each of the questions one by one
-    for (let i = 0; i < 10; i++) {
-
-        print.append('<hr class="my-2"><p class="question">"' + quotes[i] + '"</p>');
-
-        //once the question has been appended, we append the radio buttons
-        //we are using conditionals to make sure the buttons are set with accurate IDs regarding their value.
-        if (khalidQuote[i]) {
-            print.append('<form>    <input type="radio" name="q'+i+'" value="right"> DJ Khaled    <input type="radio" name="q'+i+'" value="wrong"> US President </form>')
-        } else {
-            print.append('<form>    <input type="radio" name="q'+i+'" value="wrong"> DJ Khaled    <input type="radio" name="q'+i+'" value="right"> US President </form>')
+    //This function is called upon within the countdown function every second, 
+    //it decreases and prints the current time left, and signals the end of the game when the time left is zero.
+    function decrement() {
+        timeLeft--;
+        $("#clock").text(timeLeft)
+        if (timeLeft ===0) {
+            clearInterval(intervalId);
+            endGame();
         }
     }
 
-    //prints the button to mark completion of the quiz
-    print.append('<input type="submit" value="Submit Answers" id="finish">');
+    //This event covers the start of the game, upon click of the start button.
+    $("#start").on("click", function() {
+        //removes button and preps area for more content
+        print.empty();
 
-    //This event is called upon clicking the finish button during the games run.
-    //Stops the timer to prevent the endgame function from running a second time,
-    //signals the end of the game
-    $("#finish").on("click", function() {
-        event.preventDefault()
-        console.log('This button works')
-        clearInterval(intervalId);
-        endGame();
+        //prints the countdown timer and provides an id for the decrement function to overwrite.
+        print.append('<h2>Time Left: <span id="clock">90</span></h2>');
+
+        //this for loop will print each of the questions one by one
+        for (let i = 0; i < 10; i++) {
+
+            print.append('<hr class="my-2"><p class="question">"' + quotes[i] + '"</p>');
+
+            //once the question has been appended, we append the radio buttons
+            //we are using conditionals to make sure the buttons are set with accurate IDs regarding their value.
+            if (khalidQuote[i]) {
+                print.append('<form>    <input type="radio" name="q'+i+'" value="right"> DJ Khaled    <input type="radio" name="q'+i+'" value="wrong"> US President </form>')
+            } else {
+                print.append('<form>    <input type="radio" name="q'+i+'" value="wrong"> DJ Khaled    <input type="radio" name="q'+i+'" value="right"> US President </form>')
+            }
+        }
+
+        //prints the button to mark completion of the quiz
+        print.append('<hr class="my-2"><input type="submit" value="Submit Answers" id="finish">');
+
+        //This event is called upon clicking the finish button during the games run.
+        //Stops the timer to prevent the endgame function from running a second time,
+        //signals the end of the game
+        $("#finish").on("click", function() {
+            event.preventDefault()
+            clearInterval(intervalId);
+            endGame();
+        })
+
+        //Countdown for the quiz begins
+        countdown();
     })
 
-    //Countdown for the quiz begins
-    countdown();
-})
+    //This function tells the game what to do when it ends.
+    function endGame() {
 
-//This function tells the game what to do when it ends.
-function endGame() {
+        //Goes through each of the questions
+        for (let i = 0; i < 10; i++) {
 
-    for (let i = 0; i < 10; i++) {
+            var thisResult = $('input[name="q'+i+'"]:checked').val();
 
-        var thisResult = $('input[name="q'+i+'"]:checked').val();
-        console.log(thisResult)
-        if (thisResult === "right") {
-            correct++;
-        } else if (thisResult === "wrong") {
-            incorrect++;
-        } else {
-            unanswered++;
+            //You can check the console log for which answers were correct or incorrect
+            console.log(i + 1 +". " + thisResult)
+
+            //totals the scores for the right, wrong, and unanswered
+            if (thisResult === "right") {
+                correct++;
+            } else if (thisResult === "wrong") {
+                incorrect++;
+            } else {
+                unanswered++;
+            }
         }
+
+        //clears the main container
+        print.empty();
+        
+        //prints the results onto the screen
+        print.append('<p> Answered Correctly: ' + correct + '</p>');
+        print.append('<p> Answered Incorrectly: ' + incorrect + '</p>');
+        print.append('<p> Left Unanswered: ' + unanswered + '</p>');
     }
 
-    print.empty();
-    console.log("Correct: " + correct)
-    console.log("incorrect: " + incorrect)
-    console.log("Left Blank: " + unanswered)
-    print.append('<p> Answered Correctly: ' + correct + '</p>');
-    print.append('<p> Answered Incorrectly: ' + incorrect + '</p>');
-    print.append('<p> Left Unanswered: ' + unanswered + '</p>');
-}
+});
