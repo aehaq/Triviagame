ac28a04252306d5a08333ef5dd828b6cf83721af
aehaq <azfarehaq@gmail.com>
2018-08-08 22:49:53 -0700

    created lists for quotes and quote status

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+var quotes = [
+    "They kick you when you\'re down, but they wanna kick it when you\'re up.",
+    "Keep your eyes on the stars and your feet on the ground.",
+    "All that I am, or hope to be I owe to my angel mother",
+    "The object of love is to serve, not to win.",
+    "I always make my dreams into goals.",
+    "You can do what you have to do, and sometimes you can do it better than you think.",
+    "I am a slow walker, but I never walk back.",
+    "The key is to vote because we need a vote to put the people in power that we want to represent us." ,
+    "Without passion you don't have energy, without energy you have nothing.",
+    "You can never run out of keys."
+];
+
+var quotesKhalid = [
+    true, 
+    false, 
+    false, 
+    false, 
+    true, 
+    false, 
+    false, 
+    true, 
+    false, 
+    true
+];
+
+
